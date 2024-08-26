c8f688a512fe0fbb970cdd30d2c58ba21b37608d
Shucharjer <2592284636@qq.com>
2024-08-26 17:36:33 +0800

    fixed login by thrid party

--- my-project-backend/src/main/java/com/example/entity/dto/Account.java
+++ my-project-backend/src/main/java/com/example/entity/dto/Account.java
@@ -32,6 +32,6 @@ public class Account implements BaseData {
     double token;
     @TableField("openId")
     String openId;
-    @TableField("giteeId")
+    @TableField("githubId")
     Integer giteeId;
 }
